698e0bea3c5342de8a92ed9c5ff9a2171da6e579
Demitri Plessas <demitri.plessas@gmail.com>
2017-06-19 13:18:13 -0500

    An R script that generates the ranking.
    Initial commit. Generates a ranking of title fight boxers by a modified PageRank algorithm, In this version, all wins are considered to have the same relative weight regardless of the type of win.

---
+++ ranking_boxers.R
@@ -0,0 +1,35 @@
+# Generate the rankings for "A Search for Champion Boxers".
+#
+#
+# Boxer Rankings for all title fight boxers in the five major boxing leagues 
+# (WBA, WBC, WBO, IBF, IBO).
+# 
+# Data source: BoxRec.com
+
+library(RSpectra)
+library(dplyr)
+library(tidyr)
+library(docstring)
+source('ranking_functions.R')
+
+# Remove website url from boxerid.
+CleanBoxerID <- function(boxerID){
+  return(gsub("http://boxrec.com/boxer/", "", boxerID))
+}
+
+boxing.df <- read_csv("Data/AllLeagues.csv")
+boxers.df <- read_csv("Data/BoxerInfoByAttributes.csv")
+boxers.df$boxerid <- sapply(boxers.df$boxerid, CleanBoxerID)
+# Generate the ranking.
+ranked.boxers <- RankBoxers(boxing.df)
+# Add additional information for visualizations.
+ranked.boxers <- merge(ranked.boxers, 
+                       boxers.df[, c("boxerid", "boxername", "alias",
+                                     "division", "bouts", "KOs", "rounds",
+                                     "stance", "debut")], 
+                       by.x = 'boxerid', 
+                       by.y = 'boxerid')
+# Order the data frame by rankings.
+ranked.boxers <- ranked.boxers[
+  order(ranked.boxers$eigenvecscore, decreasing=TRUE),]
+write_csv(ranked.boxers, "Data/StandardRanking.csv")
